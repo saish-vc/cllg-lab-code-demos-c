a64fc276ce29a9b915a738260a51eaf352ac1461
saish-vc <sunilsawant6812@gmail.com>
2026-04-26 23:10:45 +0530

    first c program

---
+++ pract1.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+
+    printf("---welcome to c programming lab---\n");
+    printf("---HAPPY LEARNING---\n");
+    printf("NAME: RAHUL SHARMA");
+    printf("Department: Computer Science");
+    printf("Roll No: MNTC0825001");
+
+    return 0;
+
+}
