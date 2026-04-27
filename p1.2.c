adc238aa9a6a7788379945a225eacc93b2699987
[saish sawant] <[sunilsawant6812@gmail.com]>
2026-04-27 12:50:58 +0530

    updated 1.2.c

--- p1.2.c
+++ p1.2.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
-int main() {
-      
-       int totalClasses, attendedClasses;
-       float attendancePercentage;
-
-
-
-    return 0;
+int main()
+{
+ int totalClasses, attendedClasses;
+ float attendancePercentage;
+ printf ("Enter total number of classes conducted: ");
+ scanf ("%d", &totalClasses);
+ printf("Enter number of classes attended by the student: ");
+ scanf("%d", &attendedClasses);
+ attendancePercentage = ((float)attendedClasses / totalClasses) * 100;
+ printf("\n--- Attendance Report ---\n");
+ printf("Total Classes Conducted: %d\n", totalClasses);
+ printf("Classes Attended: %d\n", attendedClasses);
+ printf("Attendance Percentage: %.2f%%\n", attendancePercentage);
+ if (attendancePercentage >= 75.0)
+{
+ printf("Eligibility: Eligible to write the examination\n");
+ } else {
+ printf("Eligibility: Not Eligible to write the examination\n");
+ }
+ return 0;
 }
